d5d92af55fc2b14515f95dec2d8e1f3bac1c7948
nr <nojus.rg@gmail.com>
2020-09-14 20:51:09 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,1 +1,16 @@
-print("asdf")
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def func(x):
+    return (0.47*pow(x, 4)) + (1.86 * pow(x, 3)) - (1.01 * pow(x, 2)) - (6.39 * x) - 1.85
+
+
+t = np.arange(0.0, 10, 0.01)
+s = func(t)
+
+fig, ax = plt.subplots()
+ax.plot(t, s)
+ax.grid()
+plt.show()
+print(func(1))
