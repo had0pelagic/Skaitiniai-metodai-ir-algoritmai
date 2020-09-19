70c80d367395e8eb5ba7815f9fe81c98da30ef39
nr <nojus.rg@gmail.com>
2020-09-19 21:56:31 +0300

    Newton method done

--- lab1.py
+++ lab1.py
@@ -18,8 +18,9 @@ def g(x):
     return np.e**x + np.e**-x - 100*np.sin(x)**2
 
 
-def show_graph(x, func, xlim, ylim, xline1, xline2, xline3, xline4, intervals, col, points):
+def show_graph(x, func, xlim, ylim, accurate_est, rough_est, intervals, col, points, title):
     plt.plot(x, func(x), col)
+    plt.title(title)
     plt.xlim(xlim)
     plt.ylim(ylim)
     plt.axvline(0, linewidth=0.5, color="black")
@@ -28,18 +29,31 @@ def show_graph(x, func, xlim, ylim, xline1, xline2, xline3, xline4, intervals, c
 
     for point in points:  # graph points
         plt.scatter(point, 0, color="green", s=10)
-
-    if(xline1 != -999):
-        plt.axvline(xline1, 0, color="red")  # tikslus
-        plt.axvline(xline2, 0, color="red")  # tikslus
-        plt.axvline(xline3, 0, color="yellow")  # grubus
-        plt.axvline(xline4, 0, color="yellow")  # grubus
-
-    # intervals
-    intervals.sort()  # remove
+    for a in accurate_est:
+        plt.axvline(a, 0, color="red")
+    for r in rough_est:
+        plt.axvline(r, 0, color="yellow")
     for i in intervals:
         plt.axvline(i[0], 0, linewidth=0.6, color="purple")
         plt.axvline(i[1], 0, linewidth=0.6, color="purple")
+
+    plt.grid()
+    plt.show()
+
+
+def show_graph_simple(x, func, xlim, ylim, accurate_est, rough_est, col, title):
+    plt.plot(x, func(x), col)
+    plt.title(title)
+    plt.xlim(xlim)
+    plt.ylim(ylim)
+    plt.axvline(0, linewidth=0.5, color="black")
+    plt.axhline(0, linewidth=0.5, color="black")
+    plt.scatter(0, 0, color="black")
+
+    for a in accurate_est:
+        plt.axvline(a, 0, color="red")
+    for r in rough_est:
+        plt.axvline(r, 0, color="yellow")
 
     plt.grid()
     plt.show()
@@ -72,27 +86,56 @@ def iteration_method(intervals):
             x = x + f(x) / alpha
             iteration += 1
             plt.plot([x, x, x + f(x) / alpha], [0, f(x), 0])
-            # print(str(iteration) + " " + str(x) + " " +
-            #       str(abs(f(x))) + " " + str(alpha))
+            print(str(iteration) + " " + str(x) + " " +
+                  str(abs(f(x))) + " " + str(alpha))
         results.append(x)
         alpha *= -1
     return results
 
 
-# show_graph(np.arange(-1, 6, 0.1), g, (-1, 6),
-#            (-4, 4), -999, 0, 0, 0)  # g(x) 2uzd
-# show_graph(np.arange(-5, 3, 0.1), f, (-10, 10),
-#            (-4, 4), -4.95744, 4.69721, -14.59574, 14.59574)  # f(x) 1uzd
+def newton_method(intervals):
+    results = []
+    accuracy = 1e-11
+    beta = 0.1  # skirtas konvergavimo taisymui (nebutinas)
+    for interval in intervals:
+        x = interval[0]
+        print("-----FINDING FOR: " + str(x) + "-----")
+        iteration = 1
+        limit = 500
+        while abs(f(x)) > accuracy and iteration < limit:
+            x = x - beta * f(x) / df(x)
+            iteration += 1
+            plt.plot([x, x, x - f(x) / df(x)], [0, f(x), 0])
+            print(str(iteration) + " " + str(x) + " " + str(abs(f(x))))
+        results.append(x)
+    return results
+
+
 acfrom = -4.95744
 acto = 4.69721
-intervals = scan_static(acfrom, acto, 0.5)
+rgfrom = -14.59574
+rgto = 14.59574
+
+accurate_est = [-4.95744, 4.69721]
+rough_est = [-14.59574, 14.59574]
+
+intervals = scan_static(acfrom, acto, 0.5)  # 3
 print("intervals: " + str(intervals))
+
 points_iteration = iteration_method(intervals)
 print("iteration method: " + str(points_iteration))
+
+points_newton = newton_method(intervals)
+print("iteration method: " + str(points_newton))
+
+# show_graph(np.arange(-5, 3, 0.1), f, (-10, 10),
+#            (-4, 4), acfrom, acto, rgfrom, rgto, intervals, "teal", points_iteration, "Iteration") # iteration 4
 show_graph(np.arange(-5, 3, 0.1), f, (-10, 10),
-           (-4, 4), -4.95744, 4.69721, -14.59574, 14.59574, intervals, "blue", points_iteration)  # f(x)
-# show_graph(np.arange(-5, 3, 0.1), df, (-10, 10),
-#            (-4, 4), -4.95744, 4.69721, -14.59574, 14.59574, intervals, "teal")  # f(x)
+           (-4, 4), accurate_est, rough_est, intervals, "teal", points_newton, "Newton")  # newton 4
+# show_graph_simple(np.arange(-5, 3, 0.1), g, (-10, 10),
+#                   (-4, 4), accurate_est, rough_est, "blue", "g(x)") # g(x) 2
+# show_graph_simple(np.arange(-5, 3, 0.1), f, (-10, 10),
+#                   (-4, 4), accurate_est, rough_est, "blue", "f(x)")  # f(x) 1
 
 # check roots
 # roots = fsolve(f, [1.54256, 2.04256])
