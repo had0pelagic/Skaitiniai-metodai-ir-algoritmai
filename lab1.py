f713291d46e105c0c60c809b526b2c25bf86d162
nr <nojus.rg@gmail.com>
2020-09-24 22:10:27 +0300

    Added print, user input, need to find roots with g(x)

--- lab1.py
+++ lab1.py
@@ -65,11 +65,11 @@ def show_graph_simple(x, func, xlim, ylim, accurate_est, rough_est, col, title):
     plt.show()
 
 
-def scan_static(acfrom, acto, step):
+def scan_static(accurate_est, step):
     intervals = []
-    first = acfrom
-    last = acto
-    while first < acto:
+    first = accurate_est[0]
+    last = accurate_est[1]
+    while first < accurate_est[1]:
         last = first + step
         if np.sign(f(first)) != np.sign(f(last)):
             # print(str(f(first)) + " " + str(f(last)) + "////////////////////")
@@ -79,61 +79,60 @@ def scan_static(acfrom, acto, step):
     return intervals
 
 
-def iteration_method(intervals, f, alpha, volumeExercise):
+def iteration_method(intervals, f, alpha):
     results = []
-    # alpha = 1000  # 100# mod for last exercise
+    information = []
     accuracy = 1e-11
     for interval in intervals:
         x = interval[0]
-        print("-----FINDING FOR: " + str(x) + "-----")
+        # print("-----FINDING FOR: " + str(x) + "-----")
         iteration = 1
-        limit = 20000  # 200# mod for last exercise
-        print("v is equal to = " + str(np.round(f(0.32), 2)) + " need v = 0.93")
-        # prie kiekvienos saknies reikia parodyti alpha reiksme
+        limit = 200
         while abs(f(x)) > accuracy and iteration < limit:
             x = x + f(x) / alpha
             iteration += 1
             # plt.plot([x, x, x + f(x) / alpha], [0, f(x), 0])
-            print(str(iteration) + " " + str(x) + " " +
-                  str(abs(f(x))) + " " + str(alpha))
-            # if np.round(f(x), 2) == volumeExercise[1] and volumeExercise[0] == 'y':
-            #     break  # mod for last exercise
-        # if volumeExercise[0] == 'y':
-        #     results.append(np.round(x, 2))
-        # else:
-            results.append(x)
+            # print(str(iteration) + " " + str(x) + " " +
+            #       str(abs(f(x))) + " " + str(alpha))
+        results.append(x)
+        information.append(
+            ["iterations: " + str(iteration), "from interval: " + str(interval[0]),  "to interval: " + str(interval[1]), "alpha: " + str(alpha), "accuracy: " + str(accuracy)])
         alpha *= -1
-    return results
+    return results, information
 
 
 def newton_method(intervals, f):
     results = []
+    information = []
     accuracy = 1e-11
     beta = 0.1  # skirtas konvergavimo taisymui (nebutinas)
     for interval in intervals:
         x = interval[0]
-        print("-----FINDING FOR: " + str(x) + "-----")
+        # print("-----FINDING FOR: " + str(x) + "-----")
         iteration = 1
         limit = 500
         while abs(f(x)) > accuracy and iteration < limit:
             x = x - beta * f(x) / df(x)
             iteration += 1
             plt.plot([x, x, x - f(x) / df(x)], [0, f(x), 0])
-            print(str(iteration) + " " + str(x) + " " + str(abs(f(x))))
+            # print(str(iteration) + " " + str(x) + " " + str(abs(f(x))))
         results.append(x)
-    return results
+        information.append(
+            ["iterations: " + str(iteration), "from interval: " + str(interval[0]),  "to interval: " + str(interval[1]), "accuracy: " + str(accuracy), "beta: " + str(beta)])
+    return results, information
 
 
 def scan_dynamic(intervals, start_step, f):
     accuracy = 1e-11
     results = []
+    information = []
     for interval in intervals:
         step = start_step
         x_from = interval[0]
         x_to = interval[0]
         limit = 200
         iteration = 1
-        print("-----FINDING FOR: " + str(x_from) + "------")
+        # print("-----FINDING FOR: " + str(x_from) + "------")
         while abs(f(x_from)) > accuracy and iteration < limit:
             iteration += 1
             x_from += step
@@ -141,67 +140,100 @@ def scan_dynamic(intervals, start_step, f):
                 x_from -= step
                 step /= 10
                 x_to = x_from
-            print("iteration: " + str(iteration) + " x/h: " + str(x_from) + " " +
-                  str(abs(f(x_from))))
+            # print("iteration: " + str(iteration) + " x/h: " + str(x_from) + " " +
+            #       str(abs(f(x_from))))
         results.append(x_from)
-    return results
+        information.append(
+            ["iterations: " + str(iteration), "from interval: " + str(interval[0]),  "to interval: " + str(interval[1]), "accuracy: " + str(accuracy)])
+    return results, information
 
 
-# todo: 5th point, 4th point methods require tables with information
-acfrom = -4.95744
-acto = 4.69721
-rgfrom = -14.59574
-rgto = 14.59574
+def info_print(information, intervals, title):
+    os.system("cls")
+    print(title + "\n")
+    print("INTERVALS: " + str(intervals) + "\n")
+    print("ROOTS: " + str(information[0]) + "\n")
+    it = 0
+    for info in information[1]:
+        print("---------FOUND ROOT: " +
+              str(information[0][it])+"--------------")
+        for i in info:
+            print(str(i))
+        it += 1
+    print("-----------------------------------------------------")
+
 
 accurate_est = [-4.95744, 4.69721]
 rough_est = [-14.59574, 14.59574]
 
-# intervals = scan_static(acfrom, acto, 0.5)  # 3
-# print("intervals: " + str(intervals))
+print("1 - - - iteration method")
+print("2 - - - newton method")
+print("3 - - - dynamic scan method")
+print("4 - - - volume exercise method")
+print("5 - - - show f(x) interval")
+print("6 - - - show g(x)")
 
-# points_iteration = iteration_method(
-#     intervals, f, 100, ['n', 0])  # v,f,h,df etc
-# print("iteration method: " + str(points_iteration))
+option = input()
 
-# points_newton = newton_method(intervals,f)
-# print("newton method: " + str(points_newton))
+if option == '1':
+    title = "Iteration method"
+    intervals = scan_static(accurate_est, 0.5)  # 3
 
-# points_scan_dynamic = scan_dynamic(intervals, 0.1,f)
-# print("scan method: " + str(points_scan_dynamic))
+    points_iteration = iteration_method(intervals, f, 100)
 
-# show_graph(np.arange(-5, 3, 0.1), f, (-10, 10),
-#            (-4, 4), accurate_est, rough_est, intervals, "teal", points_iteration, "Iteration")  # iteration 4
-# show_graph(np.arange(-5, 3, 0.1), f, (-10, 10),
-#            (-4, 4), accurate_est, rough_est, intervals, "teal", points_newton, "Newton")  # newton 4
-# show_graph(np.arange(-5, 3, 0.1), f, (-10, 10),
-#            (-4, 4), accurate_est, rough_est, intervals, "teal", points_scan_dynamic, "Scan dynamic")  # scan_dynamic 4
+    info_print(points_iteration, intervals, title)
 
-# show_graph_simple(np.arange(-5, 3, 0.1), g, (-10, 10),
-#                   (-4, 4), accurate_est, rough_est, "blue", "g(x)") # g(x) 2
-# show_graph_simple(np.arange(-5, 3, 0.1), f, (-10, 10),
-#                   (-4, 4), accurate_est, rough_est, "blue", "f(x)")  # f(x) 1
+    show_graph(np.arange(-5, 3, 0.1), f, (-10, 10),
+               (-4, 4), accurate_est, rough_est, intervals, "teal", points_iteration[0], title)  # iteration 4
 
-# intervalsLast = [[0.1, 1]]
-# points_iteration_volume = iteration_method(
-#     intervalsLast, V, 1000, ['y', 0.93])
+elif option == '2':
+    title = "Newton method"
+    intervals = scan_static(accurate_est, 0.5)  # 3
 
-# print("iteration method: " + str(points_iteration_volume))
+    points_newton = newton_method(intervals, f)
 
-# show_graph(np.arange(0, 1, 0.1), V, (-3, 3),
-#            (-4, 4), [-2, 2], [-3, 3], intervalsLast, "teal", points_iteration_volume, "Iteration_Volume")  # iteration 4
+    info_print(points_newton, intervals, title)
 
-intervalsLast = [[0.1, 1]]
-points_scan_dynamic_volume = scan_dynamic(
-    intervalsLast, 0.1, V)
+    show_graph(np.arange(-5, 3, 0.1), f, (-10, 10),
+               (-4, 4), accurate_est, rough_est, intervals, "teal", points_newton[0], title)  # newton 4
 
-print("iteration method: " + str(points_scan_dynamic_volume))
+elif option == '3':
+    title = "Scan dynamic method"
+    intervals = scan_static(accurate_est, 0.5)  # 3
 
-show_graph(np.arange(0, 1, 0.0000001), V, (-0.25, 3),
-           (-4, 4), [-2, 2], [-3, 3], intervalsLast, "teal", points_scan_dynamic_volume, "Scan_Dynamic_Volume")  # iteration 4
+    points_scan_dynamic = scan_dynamic(intervals, 0.1, f)
 
-# check roots
-# roots = fsolve(f, [1.54256, 2.04256])
-# print(str(roots) + "*******************")
+    info_print(points_scan_dynamic, intervals, title)
 
-# roots = fsolve(V, [0.1, 1])
-# print(str(roots) + "*******************")
+    show_graph(np.arange(-5, 3, 0.1), f, (-10, 10),
+               (-4, 4), accurate_est, rough_est, intervals, "teal", points_scan_dynamic[0], title)  # scan_dynamic 4
+
+elif option == '4':
+    title = "Scan dynamic volume"
+    intervals = [[0.1, 1]]
+
+    points_scan_dynamic_volume = scan_dynamic(intervals, 0.1, V)
+
+    info_print(points_scan_dynamic_volume, intervals, title)
+
+    show_graph(np.arange(0, 1, 0.0000001), V, (-0.25, 3),
+               (-4, 4), [-2, 2], [-3, 3], intervals, "teal", points_scan_dynamic_volume[0], title)  # iteration 4
+
+elif option == '5':
+    print("f(x)")
+    show_graph_simple(np.arange(-5, 3, 0.1), f, (-10, 10), (-4, 4),
+                      accurate_est, rough_est, "blue", "f(x)")  # f(x) 1
+elif option == '6':
+    print("g(x)")
+    show_graph_simple(np.arange(-5, 3, 0.1), g, (-10, 10),
+                      (-4, 4), accurate_est, rough_est, "blue", "g(x)")  # g(x) 2
+elif option == '7':
+    title = "g(x) with roots Iteration method "  # need all methods
+    intervals = [[0.1, 1]]
+
+    points_scan_dynamic_volume = scan_dynamic(intervals, 0.1, V)
+
+    info_print(points_scan_dynamic_volume, intervals, title)
+
+    show_graph(np.arange(0, 1, 0.0000001), V, (-0.25, 3),
+               (-4, 4), [-2, 2], [-3, 3], intervals, "teal", points_scan_dynamic_volume[0], title)  # iteration 4
